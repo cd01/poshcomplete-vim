be65f8af58d8ca479927dafc47ec4e6aeec4fd6c
cd01 <ibdcd01@Gmail.com>
2014-06-30 23:10:27 +0900

    Fix test/test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,1 @@
-/c/projects/appveyor-study/vim74-kaoriya-win64/vim.exe -Nu /c/projects/poshcomplete-vim/test/appveyor-vimrc -c 'Vader! C:\projects\poshcomplete-vim\test\*.vader' > /dev/null
+/c/projects/poshcomplete-vim/vim74-kaoriya-win64/vim.exe -Nu /c/projects/poshcomplete-vim/test/appveyor-vimrc -c 'Vader! C:\projects\poshcomplete-vim\test\*.vader' > /dev/null
